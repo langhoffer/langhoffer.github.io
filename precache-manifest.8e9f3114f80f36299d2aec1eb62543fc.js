61e3f9ad6c4963ddefb64bd735f4d7aff1a81a2b
Langhoffer <michal.langhoffer@gmail.com>
2019-11-06 20:09:11 +0100

    Add app

---
+++ precache-manifest.8e9f3114f80f36299d2aec1eb62543fc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f00fa863a73acf7897470e295a2fd40",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9e553645ab47a1748e10",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "869d2b87327beb35a97a",
+    "url": "/static/js/2.099c4410.chunk.js"
+  },
+  {
+    "revision": "9e553645ab47a1748e10",
+    "url": "/static/js/main.0b1c77b2.chunk.js"
+  },
+  {
+    "revision": "2e4bc1b3c022e2e6153b",
+    "url": "/static/js/runtime-main.d54b910c.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
